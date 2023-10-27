4cbc5fc95f577941d5b5dcc456da61ce1f4a902f
S. Langnes <36555973+Slangnes@users.noreply.github.com>
2023-10-27 20:21:40 +0000

    spun out html generation, and fixed for streaming

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html');
-    res.write(`
+    const htmlContent = getHtmlContent();
+    writeChunk(res, htmlContent, () => {
+        console.log('Finished writing HTML content to response.');
+        res.end();
+    });
+});
+
+server.listen(3008, () => {
+    console.log('Server is listening on port 3008');
+});
+
+function getHtmlContent() {
+    return `
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -26,7 +38,7 @@ const server = http.createServer((req, res) => {
                 @media (prefers-color-scheme: dark) {
                     body {
                         background-color: black;
-                        color: white
+                        color: white;
                     }
                 }
                 h1 {
@@ -58,7 +70,7 @@ const server = http.createServer((req, res) => {
             <li>Implemented WordPress updates through code review and process improvement.</li>
         </ul>
     
-	<h3>Consultant (Art & Tech Integration 2020-2022)</h3>
+    <h3>Consultant (Art & Tech Integration 2020-2022)</h3>
     <ul>
         <li>Collaborated with organizations such as Artlanta, Chase Village, and Art & Industry Atlanta.</li>
         <li>Facilitated coding tutorials and AR experiences at Arcadia.</li>
@@ -100,49 +112,52 @@ const server = http.createServer((req, res) => {
         </ul>
         
         <h2>Skills:</h2>
-<p>
-    <strong>Languages:</strong>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">JavaScript</a>,
-    <a href="https://www.python.org/doc/" target="_blank">Python</a>,
-    <a href="https://www.php.net/docs.php" target="_blank">PHP</a>,
-    <a href="https://www.typescriptlang.org/docs/" target="_blank">TypeScript</a><br>
-    
-    <strong>Front-End:</strong>
-    <a href="https://reactjs.org/docs/getting-started.html" target="_blank">React</a>,
-    <a href="https://vuejs.org/v2/guide/" target="_blank">Vue</a>,
-    <a href="https://vitejs.dev/guide/" target="_blank">Vite</a>,
-    <a href="https://svelte.dev/tutorial" target="_blank">Svelte</a>,
-    <a href="https://nextjs.org/docs/getting-started" target="_blank">Next.js</a>,
-    <a href="https://tailwindcss.com/docs" target="_blank">Tailwinds</a><br>
-    
-    <strong>Back-End:</strong>
-    <a href="https://nodejs.org/en/docs/" target="_blank">Node</a>,
-    <a href="https://flask.palletsprojects.com/en/2.0.x/" target="_blank">Flask</a>,
-    <a href="https://fastapi.tiangolo.com/" target="_blank">FastAPI</a><br>
-    
-    <strong>Databases:</strong>
-    <a href="https://www.postgresql.org/docs/" target="_blank">Postgres</a>,
-    <a href="https://docs.mongodb.com/" target="_blank">MongoDB</a>,
-    <a href="https://docs.microsoft.com/en-us/sql/sql-server/documentation?view=sql-server-ver15" target="_blank">Windows SQL Server</a>,
-    <a href="https://dev.mysql.com/doc/" target="_blank">MySQL</a><br>
-    
-    <strong>Tools & Environment:</strong>
-    <a href="https://docs.docker.com/" target="_blank">Docker</a>,
-    <a href="https://www.jenkins.io/doc/" target="_blank">Jenkins</a>,
-    <a href="https://git-scm.com/doc" target="_blank">Git</a>,
-    <a href="https://httpd.apache.org/docs/" target="_blank">Apache</a>,
-    <a href="https://www.linux.org/pages/manuals/" target="_blank">Linux</a><br>
-    
-    <strong>Others:</strong>
-    <a href="https://docs.retool.com/docs" target="_blank">Retool</a>,
-    <a href="https://wordpress.org/support/documentation/" target="_blank">WordPress</a>
-</p>
+    <p>
+        <strong>Languages:</strong>
+        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">JavaScript</a>,
+        <a href="https://www.python.org/doc/" target="_blank">Python</a>,
+        <a href="https://www.php.net/docs.php" target="_blank">PHP</a>,
+        <a href="https://www.typescriptlang.org/docs/" target="_blank">TypeScript</a><br>
+        
+        <strong>Front-End:</strong>
+        <a href="https://reactjs.org/docs/getting-started.html" target="_blank">React</a>,
+        <a href="https://vuejs.org/v2/guide/" target="_blank">Vue</a>,
+        <a href="https://vitejs.dev/guide/" target="_blank">Vite</a>,
+        <a href="https://svelte.dev/tutorial" target="_blank">Svelte</a>,
+        <a href="https://nextjs.org/docs/getting-started" target="_blank">Next.js</a>,
+        <a href="https://tailwindcss.com/docs" target="_blank">Tailwinds</a><br>
+        
+        <strong>Back-End:</strong>
+        <a href="https://nodejs.org/en/docs/" target="_blank">Node</a>,
+        <a href="https://flask.palletsprojects.com/en/2.0.x/" target="_blank">Flask</a>,
+        <a href="https://fastapi.tiangolo.com/" target="_blank">FastAPI</a><br>
+        
+        <strong>Databases:</strong>
+        <a href="https://www.postgresql.org/docs/" target="_blank">Postgres</a>,
+        <a href="https://docs.mongodb.com/" target="_blank">MongoDB</a>,
+        <a href="https://docs.microsoft.com/en-us/sql/sql-server/documentation?view=sql-server-ver15" target="_blank">Windows SQL Server</a>,
+        <a href="https://dev.mysql.com/doc/" target="_blank">MySQL</a><br>
+        
+        <strong>Tools & Environment:</strong>
+        <a href="https://docs.docker.com/" target="_blank">Docker</a>,
+        <a href="https://www.jenkins.io/doc/" target="_blank">Jenkins</a>,
+        <a href="https://git-scm.com/doc" target="_blank">Git</a>,
+        <a href="https://httpd.apache.org/docs/" target="_blank">Apache</a>,
+        <a href="https://www.linux.org/pages/manuals/" target="_blank">Linux</a><br>
+        
+        <strong>Others:</strong>
+        <a href="https://docs.retool.com/docs" target="_blank">Retool</a>,
+        <a href="https://wordpress.org/support/documentation/" target="_blank">WordPress</a>
+    </p>
     </body>
     </html>
-    `);
-    res.end();
-});
+    `;
+}
 
-server.listen(3008, () => {
-    console.log('Server is listening on port 3008');
-});
+function writeChunk(res, data, cb) {
+    if (!res.write(data)) {
+        res.once('drain', cb);
+    } else {
+        process.nextTick(cb);
+    }
+}
